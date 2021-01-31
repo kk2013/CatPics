1f102bde09670a4d7997ac0349110d598864bc5a
Jo Santa Maria <kk2013@users.noreply.github.com>
2021-01-31 10:43:54 +0000

    Make Breed and Category optional

--- app/src/main/java/com/catpics/model/Cat.kt
+++ app/src/main/java/com/catpics/model/Cat.kt
@@ -1,8 +1,8 @@
 package com.catpics.model
 
 data class Cat(
-    val breeds: List<Breed>,
-    val categories: List<Category>,
+    val breeds: List<Breed>?,
+    val categories: List<Category>?,
     val id: String,
     val url: String
 )
